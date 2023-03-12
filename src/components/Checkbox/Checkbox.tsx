a4919adaece7a4e79bc61ddacd3f687f9d98788f
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-03-12 23:58:33 +0000

    removed light and dark theme of the checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -9,13 +9,10 @@ export default function Checkbox(props: CheckboxProps) {
 				id={id}
 				type="checkbox"
 				value={props.checked ? "true" : "false"}
-				className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				className="w-4 h-4 text-blue-600 rounded focus:ring-blue-600 ring-offset-gray-800 focus:ring-2 bg-gray-700 border-gray-600"
 				onChange={props.onChange}
 			/>
-			<label
-				htmlFor={id}
-				className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-			>
+			<label htmlFor={id} className="ml-2 text-sm font-medium  text-gray-300">
 				{props.label}
 			</label>
 		</div>
--- src/components/SubmitButton/SubmitButton.tsx
+++ src/components/SubmitButton/SubmitButton.tsx
@@ -4,7 +4,7 @@ export default function SubmitButton(props: ButtonProps) {
 	return (
 		<button
 			type="button"
-			className="py-2.5 px-5 mb-2 mt-6 font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+			className="py-2.5 px-5 mb-2 mt-6 font-medium focus:outline-none  rounded-lg border  focus:z-10 focus:ring-4 focus:ring-gray-700 bg-gray-800 text-gray-400 border-gray-600 hover:text-white hover:bg-gray-700 transition-all duration-300 ease-in-out"
 			onClick={props.onClick}
 		>
 			{props.text}
